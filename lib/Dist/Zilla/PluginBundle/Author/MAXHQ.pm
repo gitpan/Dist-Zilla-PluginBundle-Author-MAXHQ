cf385d97a5e27d8e14b2db0aa77f4147259ae534
Jens Berthold <maxhq@cpan.org>
2013-11-29 11:46:20 +0000

    Import of MAXHQ/Dist-Zilla-PluginBundle-Author-MAXHQ-0.011002 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-Author-MAXHQ
    gitpan-cpan-version:      0.011002
    gitpan-cpan-path:         MAXHQ/Dist-Zilla-PluginBundle-Author-MAXHQ-0.011002.tar.gz
    gitpan-cpan-author:       MAXHQ
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/PluginBundle/Author/MAXHQ.pm
+++ lib/Dist/Zilla/PluginBundle/Author/MAXHQ.pm
@@ -1,7 +1,7 @@
 use strict;
 package Dist::Zilla::PluginBundle::Author::MAXHQ;
 # ABSTRACT: Dist::Zilla like MAXHQ when you build your dists
-our $VERSION = '0.011001'; # VERSION
+our $VERSION = '0.011002'; # VERSION
 
 
 use Moose;
@@ -18,95 +18,88 @@ sub configure {
  
     $self->add_plugins(
 		#
-		# Before build
+		# Files included
 		#
-        [ 'PromptIfStale' => 'build' => { phase => 'build', module => [ blessed($self) ] } ],
-        
-		#
-		# Collect files, calculate dependencies...
-		#
-		['GatherDir' => {
-			exclude_match => '^[^\.]+\.ini',
-			exclude_match => '^[^\.]+\.html',
-			exclude_match => '^[^\.]+\.tar\.gz',
+		['GatherDir' => {              # skip files on top level
+			exclude_match => [
+				'^[^\.]+\.ini$',
+				'^[^\.]+\.html$',
+				'^[^\.]+\.tar\.gz$',
+			]
 		}],
-		# produce a META.yml
-		'MetaYAML',
+		'PruneCruft',                  # prune stuff you probably don't want
 		
-		# build a Build.PL that uses Module::Build to install the distribution
-		'ModuleBuild',
-		
-		# prune stuff that you probably don't mean to include in the package
-		'PruneCruft',
-		
-		# automatically extract prereqs from your modules
-		'AutoPrereqs',
-		
-		# manually add some prereqs
-		['Prereqs' => {
-			'Test::Inline' => 0,      # for =inc::ExtractInlineTests
-			'File::Find::Rule' => 0,  # for =inc::ExtractInlineTests
+		['ExecDir' => {                # install contents of bin/ as executable
+			dir => 'bin',
+		}],		
+		['ShareDir' => {               # include all files in /share
+			dir => 'share',
 		}],
 		
-		# "Automatically cleans up the mess from other Prereq modules"
-		'PrereqsClean',
-		
-		# adjust prereqs to use latest version available
-		'LatestPrereqs',
-		
-		# install contents of bin/ as executable
-		'ExecDir',
-		
 		#
-		# Release
+		# Conversion and replacements
 		#
-
-		# Replace variable {{$NEXT}} in "Changes" file with new version and date
-		['NextRelease' => { format => '%-9v %{yyyy-MM-dd}d' }],
-		
-		# Test release
-		'TestRelease',
-		
-		# ask for confirmation
-		'ConfirmRelease',
-
-		# Like calling "dzil clean" after release to remove builds
-		'Clean',
-		
-		#
-		# Auto generation
-		#
-		
-		# Automatically insert version variable (replace "# VERSION" by "our $VERSION = '...';")
-		'OurPkgVersion',
+		'OurPkgVersion',               # replaces "# VERSION" by "our $VERSION = '...';"
+		['NextVersion::Semantic' => {  # generate next version based on type of changes
+			major => 'NEW FEATURES, API CHANGES',
+			minor => 'ENHANCEMENTS',
+			revision => 'REVISION, BUG FIXES, DOCUMENTATION',
+		}],
+		'PreviousVersion::Changelog',  # fetch previous version from changelog
+		                               # alternatively run: V=0.00100 dzil release
 		
 		# weave your Pod together from configuration and Dist::Zilla
 		# (turns "# ABSTRACT" into POD, processes =method and short lists etc.)
 		# To exclude files from PodWeaver see: http://blogs.perl.org/users/polettix/2011/11/distzilla-podweaver-and-bin.html
 		'PodWeaver',
+
+		#
+		# Prerequisites
+		#
+		'AutoPrereqs',                 # extract prereqs from your modules
+		'Prereqs::AuthorDeps',         # "adds Dist::Zilla and the result of "dzil
+		                               # authordeps" to the 'develop' phase prerequisite list"
+		'PrereqsClean',                # "Cleans up mess from other Prereq modules"
+		['Prereqs::MatchInstalled::All' => { # adjust prereqs to use latest version available
+			exclude => [qw( strict warnings )]
+		}],
 		
-		# output a LICENSE file
-		'License',
-		
-		# build a README file citing the dist's name, version, abstract, and license
-		'ReadmeAnyFromPod',
-		
-		# extract inline test code from modules
-		'Test::Inline',
+		#
+		# Auto generation --- distribution files
+		#
+		'ModuleBuild',                 # build a Build.PL that uses Module::Build to install the distribution
+		'MetaYAML',                    # generate META.yml
+		'Manifest',                    # generate Manifest (list of all files)
+		'License',                     # generate LICENSE
+		'ReadmeAnyFromPod',            # generate README (with dist's name, version, abstract, license)
+
+		#
+		# Auto generation --- tests
+		#
+		'Test::Inline',                # generate .t files from inline test code (POD)
 		
 		# auto-generate various tests
 		'ExtraTests',
 		'Test::Perl::Critic',
 		'PodCoverageTests',
 		'PodSyntaxTests',
-		
-		# Generate list of all files in the distribution
-		'Manifest',
-		
-		# Generate HTML documentation
-		['Pod2Html' => {
+
+		#
+		# Auto generation --- docs
+		#
+		['Pod2Html' => {               # generate HTML documentation
 			'dir' => 'doc',
 		}],
+
+		#
+		# Release
+		#
+		['NextRelease' => {            # replace {{$NEXT}} in "Changes" file with new version and date
+			format => '%-9v %{yyyy-MM-dd}d',
+		}],
+		'TestRelease',                 # test before releasing
+		'ConfirmRelease',              # ask for confirmation before releasing
+		'Clean',                       # Like "dzil clean" after release
     );
 }
  
@@ -124,7 +117,7 @@ Dist::Zilla::PluginBundle::Author::MAXHQ - Dist::Zilla like MAXHQ when you build
 
 =head1 VERSION
 
-version 0.011001
+version 0.011002
 
 =head1 SYNOPSIS
 
